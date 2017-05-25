428094f8d4be3eb3a5333c151acc24201460eac0
Paul Water <p.r.water@setbased.nl>
2017-05-25 10:44:14 +0200

    First commit.

---
+++ src/CanonicalHostnameResolver.php
@@ -0,0 +1,26 @@
+<?php
+//----------------------------------------------------------------------------------------------------------------------
+namespace SetBased\Abc\DomainResolver;
+
+//----------------------------------------------------------------------------------------------------------------------
+/**
+ * Interface for CanonicalHostnameResolvers. A CanonicalHostnameResolver resolves the preferred hostname (a.k.a. fully
+ * qualified domain name (FQDN)) of a website.
+ */
+interface CanonicalHostnameResolver
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the canonical hostname (a.k.a. preferred fully qualified domain name).
+   *
+   * @return string
+   *
+   * @api
+   * @since 1.0.0
+   */
+  public function getCanonicalHostname();
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
